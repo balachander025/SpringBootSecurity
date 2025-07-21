9d92f76f0a73d9f4df19a7adb6f09ad7650bcebd
Balachander <166808771+balachander025@users.noreply.github.com>
2025-07-21 18:32:08 +0530

    Create HomeController.java

---
+++ HomeController.java
@@ -0,0 +1,24 @@
+package com.bala.Myspringboot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String home(){
+        return "home";
+    }
+    @GetMapping("/login")
+    public String login(){
+        return "login";
+    }
+    @GetMapping("/user")
+    public String user(){
+        return "user";
+    }
+    @GetMapping("/admin")
+    public String admin(){
+        return "admin";
+    }
+}
